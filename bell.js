4af764e3968d5dc0dccf49c4d51b90f992461ce8
hit9 <nz2324@126.com>
2014-09-15 12:07:44 +0800

    v0.3.1

--- bell.js
+++ bell.js
@@ -49,7 +49,7 @@ var log = util.log;
 co(function *(){
   // argv parsing
   program
-  .version('0.3.0')
+  .version('0.3.1')
   .usage('<service> [options]')
   .option('-c, --configs-path <c>', 'configs file path')
   .option('-s, --sample-configs', 'generate sample configs file')
