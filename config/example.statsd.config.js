b694d6a6c4e1c140dbc89928e2b8ff9533a0108d
hit9 <nz2324@126.com>
2014-09-19 14:18:53 +0800

    typo

--- config/example.statsd.config.js
+++ config/example.statsd.config.js
@@ -1,8 +1,8 @@
 /**
  * Example configuration for statsd to work with node-bell.
  *
- * Just append 'node-bell' to your backends, and then set configuration
- * items, all avaliable config items:
+ * Just append 'node-bell' to statsd backends, set config items, and
+ * then restart statsd. Avaliable config items:
  *
  *   bellHost, default: '0.0.0.0'
  *   bellPort, default: 8889
