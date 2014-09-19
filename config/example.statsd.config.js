734f5a0ec95e659dbcc0c1636fa9f9a50d358699
hit9 <nz2324@126.com>
2014-09-19 14:16:33 +0800

    single quote

--- config/example.statsd.config.js
+++ config/example.statsd.config.js
@@ -1,6 +1,18 @@
+/**
+ * Example configuration for statsd to work with node-bell.
+ *
+ * Just append 'node-bell' to your backends, and then set configuration
+ * items, all avaliable config items:
+ *
+ *   bellHost, default: '0.0.0.0'
+ *   bellPort, default: 8889
+ *   bellIgnores, default: ['statsd.*']
+ *   bellTimerDataFields, default: ['mean', 'count_ps']
+ */
+
 {
   port: 8125
-, backends: ["node-bell"]
-, bellHost: "0.0.0.0"
+, backends: ['node-bell']
+, bellHost: '0.0.0.0'
 , bellPort: 8889
 }
