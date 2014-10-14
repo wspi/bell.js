8eea3a7bc143ec9b1faf39825c764e1ee28c8bbc
hit9 <nz2324@126.com>
2014-10-14 19:10:59 +0800

    untested go client

--- clients/client.go
+++ clients/client.go
@@ -2,3 +2,27 @@
 // Copyright (c) 2014 Eleme, Inc. https://github.com/eleme/node-bell
 
 package node-bell
+
+
+import (
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	Host string
+	Port int
+	Sock nil
+}
+
+
+func New(host string, port int) *Client {
+	client := Client{Host: host, Port: port}
+	return &client
+}
+
+
+func (client *Client) Connect() {
+	address := fmt.Sprintf("%s:%d", client.Host, client.Port)
+	client.Sock = net.Dail("tcp", address)
+}
