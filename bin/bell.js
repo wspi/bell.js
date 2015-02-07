183968a7d598d7249062591b09d7d024752d18fa
hit9 <hit9@icloud.com>
2015-02-08 02:46:45 +0800

    v0.6.4

--- bin/bell.js
+++ bin/bell.js
@@ -1,5 +1,5 @@
 #!/bin/sh
-':' //; exec "$(command -v node)" "--harmony-generators" "$0" "$@"
+':' //; exec "$(command -v node)" "--harmony" "$0" "$@"
 
 // about the shebang: we can't pass '--harmony' to node via /usr/bin/env
 require('../bell.js');
