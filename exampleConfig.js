19bcc6968b5aadb5ef57f0ae5367a8dd59c74910
hit9 <hit9@icloud.com>
2015-10-17 01:45:12 +0800

    Rename alerter.modules ti alerter.senders

--- exampleConfig.js
+++ exampleConfig.js
@@ -55,7 +55,7 @@
  * Alerter Options
  *   host           alerter host to bind/connect. [default: '0.0.0.0']
  *   port           alerter port to listen/connect. [default: 2017]
- *   modules        module to send message (i.e. sms, email) for alerter
+ *   senders        module to send message (i.e. sms, email) for alerter
  */
 
 {
@@ -102,6 +102,6 @@
 
   alerter: {
     port: 2017,
-    modules: [],
+    senders: [],
   },
 }
